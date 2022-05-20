740955df306e01a7250d5b5dea792c46d2632c51
ds902022 <33606227+ds902022@users.noreply.github.com>
2022-05-20 20:26:24 +0100

    fix crash, add status logging to channel

--- music.js
+++ music.js
@@ -6,8 +6,39 @@ let client = null;
 module.exports = {
     init: (mainClient) => {
         client = mainClient;
-        const player = new Player(client, { leaveOnEmpty: false });
+        const player = new Player(client, { leaveOnEnd: false });
         client.player = player;
+        client.player
+            // Emitted when channel was empty.
+            .on("channelEmpty", (queue) =>
+                queue.data.channel.send(
+                    `Everyone left the Voice Channel, queue ended.`
+                )
+            )
+            // Emitted when a song was added to the queue.
+            .on("songAdd", (queue, song) =>
+                queue.data.channel.send(`Song ${song} was added to the queue.`)
+            )
+            // Emitted when a song changed.
+            .on("songChanged", (queue, newSong, oldSong) =>
+                queue.data.channel.send(`${newSong} is now playing.`)
+            )
+            // Emitted when a first song in the queue started playing.
+            .on("songFirst", (queue, song) =>
+                queue.data.channel.send(`Started playing ${song}.`)
+            )
+            // Emitted when someone disconnected the bot from the channel.
+            .on("clientDisconnect", (queue) =>
+                queue.data.channel.send(
+                    `I was kicked from the Voice Channel, queue ended.`
+                )
+            )
+            // Emitted when there was an error in runtime
+            .on("error", (error, queue) => {
+                queue.data.channel.send(
+                    `Error: ${error} in ${queue.guild.name}`
+                );
+            });
     },
 
     run: async (command, message) => {
@@ -25,14 +56,11 @@ module.exports = {
                 play(message, guildQueue);
                 break;
             case "skip":
-                guildQueue.skip();
+                skip(message, guildQueue);
                 break;
             case "stop":
-                guildQueue.stop();
+                stop(message, guildQueue);
                 break;
-            // case "leave":
-            //     leave(message, guildQueue);
-            //     break;
             default:
                 message.channel.send("You need to enter a valid command!");
                 break;
@@ -51,9 +79,27 @@ async function play(message, guildQueue) {
         return message.channel.send("Invalid url");
     }
 
-    let queue = client.player.createQueue(message.guild.id);
+    let queue = client.player.createQueue(message.guild.id, {
+        data: { channel: message.channel },
+    });
     await queue.join(message.member.voice.channel);
     let song = await queue.play(matches[2]).catch((_) => {
         if (!guildQueue) queue.stop();
     });
+}
+
+function skip(message, guildQueue) {
+    if (guildQueue === undefined) {
+        message.channel.send("Nothing to skip");
+        return;
+    }
+    guildQueue.skip();
+}
+
+function stop(message, guildQueue) {
+    if (guildQueue === undefined) {
+        message.channel.send("Nothing to stop");
+        return;
+    }
+    guildQueue.stop();
 }
