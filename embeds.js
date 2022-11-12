c5b6ab537b3dfc2fbe0d493da37f02d14e078bd9
ds-exe <daniel@ds-exe.dev>
2022-11-12 01:53:32 +0000

    Remove duplication in embeds

--- embeds.js
+++ embeds.js
@@ -1,39 +1,41 @@
 const Discord = require("discord.js");
 const { prefix, botTitle, embedThumbnail } = require("./config.json");
 
-exports.init = async (client) => {
-    const creatorID = "74968333413257216";
-    const creator = await client.users.fetch(creatorID);
-    const userString = creator.username + "#" + creator.discriminator;
-    this.helpEmbed.setFooter({
-        text: `BOT made by @${userString}`,
-        iconURL: `https://cdn.discordapp.com/avatars/${creatorID}/${creator.avatar}`,
-    });
-    this.musicEmbed.setFooter({
-        text: `BOT made by @${userString}`,
-        iconURL: `https://cdn.discordapp.com/avatars/${creatorID}/${creator.avatar}`,
-    });
+let client = null;
+
+exports.init = async (mainClient) => {
+    client = mainClient;
+    this.helpEmbed = await generateEmbed(
+        `\`\`\`📌${prefix}music\n📌${prefix}time or ${prefix}until\n📌${prefix}now\n📌${prefix}timezone\n📌${prefix}reddit\n📌${prefix}perms\`\`\``
+    );
+    this.musicEmbed = await generateEmbed(
+        `\`\`\`📌${prefix}play\n📌${prefix}skip\n📌${prefix}stop\n📌${prefix}leave\n📌${prefix}shuffle\n📌${prefix}loop\n📌${prefix}setvolume\`\`\``
+    );
 };
 
-exports.helpEmbed = new Discord.MessageEmbed()
-    .setColor("#0099ff")
-    .setTitle(`${botTitle}`)
-    .setThumbnail(`${embedThumbnail}`)
-    .addFields({
-        name: "Commands Available Here 🔎",
-        value: `\`\`\`📌${prefix}music\n📌${prefix}time or ${prefix}until\n📌${prefix}now\n📌${prefix}timezone\n📌${prefix}reddit\n📌${prefix}perms\`\`\``,
-    });
+exports.helpEmbed = null;
 
-exports.musicEmbed = new Discord.MessageEmbed()
-    .setColor("#0099ff")
-    .setTitle(`${botTitle}`)
-    .setThumbnail(`${embedThumbnail}`)
-    .addFields({
-        name: "Commands Available Here 🔎",
-        value: `\`\`\`📌${prefix}play\n📌${prefix}skip\n📌${prefix}stop\n📌${prefix}leave\n📌${prefix}shuffle\n📌${prefix}loop\n📌${prefix}setvolume\`\`\``,
-    });
+exports.musicEmbed = null;
 
 exports.timestampEmbed = new Discord.MessageEmbed()
     .setColor("#00FFFF")
     .setTitle("Local time:")
     .setDescription(`<t:0:F>`);
+
+async function generateEmbed(commands) {
+    const creatorID = "74968333413257216";
+    const creator = await client.users.fetch(creatorID);
+    const userString = creator.username + "#" + creator.discriminator;
+    return new Discord.MessageEmbed()
+        .setColor("#0099ff")
+        .setTitle(`${botTitle}`)
+        .setThumbnail(`${embedThumbnail}`)
+        .addFields({
+            name: "Commands Available Here 🔎",
+            value: commands,
+        })
+        .setFooter({
+            text: `BOT made by @${userString}`,
+            iconURL: `https://cdn.discordapp.com/avatars/${creatorID}/${creator.avatar}`,
+        });
+}
