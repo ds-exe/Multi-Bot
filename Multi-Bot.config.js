cda684b8c275b6e1a98aac89b46185e39180d98a
ds-exe <daniel@ds-exe.dev>
2022-11-27 22:34:47 +0000

    Potential fix for restart on database change

--- Multi-Bot.config.js
+++ Multi-Bot.config.js
@@ -5,7 +5,12 @@ module.exports = {
     // Specify delay between watch interval
     watch_delay: 1000,
     // Specify which folder to ignore
-    ignore_watch: ["node_modules", ".db"],
+    ignore_watch: [
+        "node_modules",
+        "MultiDatabase.db",
+        ".db",
+        "MultiDatabase.db-journal",
+    ],
     exp_backoff_restart_delay: 100,
     out_file: "/dev/null",
     error_file: "/dev/null",
