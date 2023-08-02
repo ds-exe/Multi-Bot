7a08d997cc380d19311e9d9d7feaa37a4591d93c
ds-exe <daniel@ds-exe.dev>
2023-08-02 17:48:07 +0100

    Add filtering with static filters

--- src/contentfilter.js
+++ src/contentfilter.js
@@ -1,3 +1,29 @@
+import { deleteMessage, sendMessage } from "./utility.js";
+
 export function filterMessage(message) {
-    //console.log(message.content);
+    const filters = getFilters(message.guild.id);
+
+    for (const filter of filters) {
+        if (message.content.includes(filter)) {
+            const cleanedMessage = message.content.replace(filter, "");
+            sendMessage(
+                message,
+                `Banned word used, cleaned message:\n> ${cleanedMessage}`
+            );
+            deleteMessage(message);
+            return;
+        }
+    }
+}
+
+function getFilters() {
+    return ["🥹"];
+}
+
+export function addFilter(message, words) {
+    console.log(words);
+}
+
+export function deleteFilter(message, words) {
+    console.log(words);
 }
